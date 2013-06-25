4f31937cfbca83698d764b2d7c3094f031035967
tzheng <tzheng@andrew.cmu.edu>
2013-06-25 16:58:28 -0400

    update OnePost class, add properties

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -1,6 +1,17 @@
 package Controller;
 
+import java.util.ArrayList;
+
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion.Static;
+
+/**
+ * This is the main controller of the whole website
+ * @author taozheng
+ *
+ */
 public class MainController {
+	
+	private ArrayList<OnePost> posts;
 	
 	public static void main(String[] args) {
 		
